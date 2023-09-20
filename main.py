0a32dd11cb127989e38c8ccae3f14e064daed8a0
LeeHyunWon <qpzmal100@naver.com>
2023-09-20 15:14:24 +0900

    커밋2 빼기 기능 추가

--- main.py
+++ main.py
@@ -2,10 +2,19 @@
 def add_func(n1, n2):
     return n1+n2
 
+
+def sub_func(n1, n2):
+    return n1-n2
+
+
 ## 전역 변수부
-num = 100
-num1 = 200
+num1 = 100
+num2 = 200
+
 
 ##메인 코드부
-res = add_func(num, num1)
-print(num,'+', num1, '=', res)
+res = add_func(num1, num2)
+print(num1,'+', num2, '=', res)
+
+res = sub_func(num1, num2)
+print(num1,'-', num2, '=', res)
